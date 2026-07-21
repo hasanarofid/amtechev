fa037e4b283b843db75387a6ff4dc6842b8bf9ae
hasanarofid <hasanarofid@gmail.com>
2026-07-21 19:45:35 +0700

    fixing

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,6 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18308241649"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-18308241649');
+    </script>
+
     @if(config('analytics.gtm_id'))
         <!-- Google Tag Manager -->
         <script>(function (w, d, s, l, i) {
